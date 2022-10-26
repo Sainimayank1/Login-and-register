1468db1ed09734e269587ba37bb4687f940dab99
Mayank <mayanksaini4455@gmail.com>
2022-10-26 23:07:08 +0530

    Also add register

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './component/Home/Home';
 import Login from './component/Login/Login';
+import Register from './component/Register/Register';
 
 function App() {
   return (
     <>
       {/* <Home/> */}
-      <Login/>
+      {/* <Login/> */}
+      <Register/>
     </>
   );
 }
--- client/src/component/Register/Register.js
+++ client/src/component/Register/Register.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import './style.css'
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='register'>
+      <form className='Form Main'>
+        <h1>Register</h1>
+        <input type="text" placeholder="Your Name" autoComplete='off'></input>
+        <input type="email" placeholder="Your Email" autoComplete='off'></input>
+        <input type="password" placeholder="Your Password" autoComplete='off'></input>
+        <input type="password" placeholder="Re-enter Password" autoComplete='off'></input>
+        <div className='button'>Register</div>
+        <div>OR</div>
+        <div className='button'>Login</div>
+      </form>
+    </div>
   )
 }
 
