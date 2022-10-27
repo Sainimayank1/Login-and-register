bcd5f3e25518cb656eacf3e4ea3ce703fe1b66a0
Mayank <mayanksaini4455@gmail.com>
2022-10-27 08:56:31 +0530

    Add functionalty in login page using useState

--- client/src/component/Login/Login.js
+++ client/src/component/Login/Login.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import { useState } from 'react';
+
 import "./style.css";
 function Login() {
+  const [user,setUser] = useState(
+    {
+      email:"",
+      password:""
+    }
+  )
+
+  const handlechange = (e) =>
+  {
+    const {name , value } = e.target;
+      setUser({...user,
+        [name]:value})
+  }
+
   return (
     <div className='login'>
+    {console.log(user)}
       <form className='Form Main'>
       <h1>Login</h1>
-        <input type="email" placeholder="Enter your mail" autoComplete='off'></input>
-        <input type="password" placeholder="Enter your password" autoComplete='off'></input>
+        <input type="email" name='email' value={user.email} placeholder="Enter your mail" autoComplete='off' onChange={handlechange}></input>
+        <input type="password" name="password" value={user.password} placeholder="Enter your password" autoComplete='off' onChange={handlechange}></input>
         <div className='button'>Login</div>
         <div>OR</div>
         <div className='button'>Register</div>
